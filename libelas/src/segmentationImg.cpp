8ad1b23f31b1960bfb94e368d007b0731542b8d5
cjotade <camilo.jara@ug.uchile.cl>
2018-12-19 00:19:12 -0300

    Code is ready, still remain test in more images

--- libelas/src/segmentationImg.cpp
+++ libelas/src/segmentationImg.cpp
@@ -22,6 +22,107 @@
 using namespace std;
 using namespace cv;
 
+void buffer4imgs(Rect actualRect,vector<Rect> buffer_rect, int i_img, Point2d& meanPtTl, Point2d& meanPtBr){
+    vector<double> w;
+    double w0 = 0.1;
+    double w1 = 0.3;
+    double w2 = 0.5;
+    double wActual = 0.1;
+    w.push_back(w0);
+    w.push_back(w1);
+    w.push_back(w2);
+    w.push_back(wActual);
+
+    double sumTlx = 0; 
+    double sumTly = 0;
+    double sumBrx = 0; 
+    double sumBry = 0;  
+    int i = 0;
+    for (int j = i_img-3; j < i_img; j++) {
+        sumTlx += w[i]*(double)buffer_rect[j].tl().x;
+        sumTly += w[i]*(double)buffer_rect[j].tl().y;
+        sumBrx += w[i]*(double)buffer_rect[j].br().x;
+        sumBry += w[i]*(double)buffer_rect[j].br().y;
+        i+=1;
+    }
+    int meanTlx = (int)(w[i]*actualRect.tl().x + sumTlx);
+    int meanTly = (int)(w[i]*actualRect.tl().y + sumTly);
+    int meanBrx = (int)(w[i]*actualRect.br().x + sumBrx);
+    int meanBry = (int)(w[i]*actualRect.br().y + sumBry);
+    
+    meanPtTl.x = meanTlx;
+    meanPtTl.y = meanTly;
+    meanPtBr.x = meanBrx;
+    meanPtBr.y = meanBry;
+}
+
+void buffer3imgs(vector<Rect> buffer_rect, int i_img, Point2d& meanPtTl, Point2d& meanPtBr){
+    vector<double> w;
+    double w0 = 0.2;
+    double w1 = 0.2;
+    double w2 = 0.6;
+    w.push_back(w0);
+    w.push_back(w1);
+    w.push_back(w2);
+
+    double sumTlx = 0; 
+    double sumTly = 0;
+    double sumBrx = 0; 
+    double sumBry = 0;  
+    int i = 0;
+    for (int j = i_img-3; j < i_img; j++) {
+        //cout << w[i] << endl;
+        sumTlx += w[i]*(double)buffer_rect[j].tl().x;
+        sumTly += w[i]*(double)buffer_rect[j].tl().y;
+        sumBrx += w[i]*(double)buffer_rect[j].br().x;
+        sumBry += w[i]*(double)buffer_rect[j].br().y;
+        i+=1;
+    }
+    //cout << "sums" << endl;
+    //cout << sumTlx << endl;
+    //cout << sumTly << endl;
+    //cout << sumBrx << endl;
+    //cout << sumBry << endl;
+    int meanTlx = (int)sumTlx;
+    int meanTly = (int)sumTly;
+    int meanBrx = (int)sumBrx;
+    int meanBry = (int)sumBry;
+    
+    meanPtTl.x = meanTlx;
+    meanPtTl.y = meanTly;
+    meanPtBr.x = meanBrx;
+    meanPtBr.y = meanBry;
+}
+
+double IOU2Rects(Rect iboundingRectActual, Rect boundingRectPrev){
+    Rect interRect = iboundingRectActual & boundingRectPrev;
+    //cout << "interRect" << endl;
+    //cout << interRect.area() << endl;
+    Rect unionRect = iboundingRectActual | boundingRectPrev;
+    //cout << "unionRect" << endl;
+    //cout << unionRect.area() << endl;
+
+	double iou = double(interRect.area()) / unionRect.area();
+    cout << "iou" << endl;
+    cout << iou << endl;
+	return iou;
+}
+
+double IOU4Rects(Rect iboundingRectActual, vector<Rect>& buffer_rect, int i_img){
+    Rect interRect = iboundingRectActual & buffer_rect[i_img-3] & buffer_rect[i_img-2] & buffer_rect[i_img-1];
+    //cout << "interRect" << endl;
+    //cout << interRect.area() << endl;
+    Rect unionRect = iboundingRectActual | buffer_rect[i_img-3] | buffer_rect[i_img-2] | buffer_rect[i_img-1];
+    //cout << "unionRect" << endl;
+    //cout << unionRect.area() << endl;
+
+	double iou = double(interRect.area()) / unionRect.area();
+    cout << "iou" << endl;
+    cout << iou << endl;
+	return iou;
+}
+
+
 void filterDisparity(Mat& img){    
     double thresh = 50;
     double maxValue = 255;
@@ -41,22 +142,18 @@ void filterDisparity(Mat& img){
 }
 
 Mat blobSelection(Mat& img, Mat img_real,vector<Rect>& buffer_rect, int i_img){
-    
+    cout << "i_img" << endl;
+    cout << i_img+1 << endl;
+    cout << "img.size" << endl;
     cout << img.size << endl;
     cv::cvtColor(img, img, CV_RGB2GRAY);
-    //normalize(label, seeMyLabels, 0, 255, NORM_MINMAX, CV_8U);
-    //double min, max;
-    //Point min_loc, max_loc;
-
-    //minMaxLoc(img, &min, &max, &min_loc, &max_loc);
-    //Mat connectLabels;
+   
     threshold(img,img, 200, 255, THRESH_BINARY_INV);
     //bitwise_not(img,img);
     //connectedComponents(img,connectLabels,8,CV_32S);
-    imshow("img",img);
-    waitKey(0);
+    //imshow("img",img);
+    //waitKey(0);
 
-    
     // Set up the detector with default parameters.
     
     RNG rng(12345);
@@ -65,53 +162,82 @@ Mat blobSelection(Mat& img, Mat img_real,vector<Rect>& buffer_rect, int i_img){
     
     vector<vector<Point> > contours_poly(contours.size());
     vector<Rect> boundRect(contours.size());
-
+    cout << "boundRect.size()" << endl;
+    cout << boundRect.size() << endl;
     for(size_t i = 0; i < contours.size(); i++){
-        approxPolyDP(contours[i], contours_poly[i], 3, true);
+        approxPolyDP(contours[i], contours_poly[i], 3, true); //ver esto 3
         boundRect[i] = boundingRect(contours_poly[i]);
     }
 
     Mat drawing = Mat::zeros(img.size(), CV_8UC3);
 
     //vector<Rect> first_buff_rect;
+    Scalar color;
+    vector<double> IOUs;
+    vector<int> posBound;
+    cout << "contours.size()" << endl;
+    cout << contours.size() << endl;
     for( size_t i = 0; i< contours.size(); i++ ){
-        Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
+        color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
         int boundArea = boundRect[i].area();
         int realArea = img_real.rows * img_real.cols;
-        
-        if(boundArea != realArea && boundArea >= 3000){
-            cout << boundArea << " " <<realArea << endl;
-            if(i_img < 3){
+        cout << "boundArea realArea" << endl;
+        cout << boundArea << " " << realArea << endl;
+        if(boundArea != realArea && boundArea >= 1000){
+            cout << "boundArea realArea ENTER" << endl;
+            cout << boundArea << " " << realArea << endl;
+            if(i_img < 3 && boundArea >=3000){
                 Rect selectedRect(boundRect[i]);
                 buffer_rect.push_back(selectedRect);
+                cout << "bufer_rect.size()" << endl;
+                cout << buffer_rect.size() << endl;
                 rectangle(drawing, selectedRect.tl(), selectedRect.br(),color, 2);
+            }
+            else if (i_img >= 3){
+                //calc IOU para cada boundingRect[i] y guardo en vector
+                IOUs.push_back(IOU2Rects(boundRect[i],buffer_rect[i_img-1]));
+                posBound.push_back(i);
+            }
+        }
+        
+    }
+    if(i_img >= 3){
+        Point2d meanPtTl;
+        Point2d meanPtBr;
+        int posMaxIOU = std::distance(IOUs.begin(), std::max_element(IOUs.begin(), IOUs.end()));
+        double maxIOU = *max_element(IOUs.begin(), IOUs.end());
+        //cout << "posMaxIOU" << endl;
+        //cout << posMaxIOU << endl;
+        cout << "maxIOU" << endl;
+        cout << maxIOU << endl;
+        Rect selectedRect;
+        Rect actualRect = boundRect[posBound[posMaxIOU]];
+        bool minArea = abs(buffer_rect[i_img-1].area() - actualRect.area()) < 20000;
+        cout << "minArea" << endl;
+        cout << abs(buffer_rect[i_img-1].area() - actualRect.area()) << endl;
+
+        //elegir como condicion comparacion ious entre promedio 3 anteriores
+        double compareIOUs = IOU4Rects(actualRect,buffer_rect,i_img);
+        if(compareIOUs >= 0.3 && minArea){ //and areas
+            cout << "MAX IOU" << endl;
+            //elige de todos los bound actuales el que tenga mayor IOU con respecto del anterior
+            selectedRect = actualRect;
+        }
+        else{
+            if(buffer_rect[i_img-1].area() > actualRect.area()){
+                cout << "BUFFER4" << endl;
+                buffer4imgs(actualRect,buffer_rect,i_img,meanPtTl,meanPtBr);
             }
             else{
-                Rect actualRect(boundRect[i]);
-                int sumTlx = 0; 
-                int sumTly = 0;
-                int sumBrx = 0; 
-                int sumBry = 0;  
-                for (int j = i_img-3; i < i_img; i++) {
-                    sumTlx += buffer_rect[j].tl().x;
-                    sumTly += buffer_rect[j].tl().y;
-                    sumBrx += buffer_rect[j].br().x;
-                    sumBry += buffer_rect[j].br().y;
-                }
-                int meanTlx = (actualRect.tl().x + sumTlx)/4;
-                int meanTly = (actualRect.tl().y + sumTly)/4;
-                int meanBrx = (actualRect.br().x + sumBrx)/4;
-                int meanBry = (actualRect.br().y + sumBry)/4;
-                
-                Point2d meanPtTl(meanTlx,meanTly);
-                Point2d meanPtBr(meanBrx,meanBry);
-                Rect selectedRect(meanPtTl,meanPtBr);
-                buffer_rect.push_back(selectedRect);
-                rectangle(drawing, selectedRect.tl(), selectedRect.br(),color, 2);
+                cout << "BUFFER3" << endl;
+                buffer3imgs(buffer_rect,i_img,meanPtTl,meanPtBr);
             }
-            //Point2d centerRect(boundRect[i].tl().x+abs(boundRect[i].tl().x - boundRect[i].br().x)/2,boundRect[i].tl().y+abs(boundRect[i].tl().y - boundRect[i].br().y)/2);
-            //cout << centerRect << endl;
+            selectedRect = Rect(meanPtTl,meanPtBr);
         }
+        buffer_rect.push_back(selectedRect);
+        rectangle(drawing, selectedRect.tl(), selectedRect.br(),color, 2);
+        //asignar el boundingRect que tenga mayor IOU del vector y buffer entre 3 anteriores
+        
     }
     Mat result;
     addWeighted(img_real, 0.5, drawing, 0.5, 0.0, result);
@@ -120,6 +246,8 @@ Mat blobSelection(Mat& img, Mat img_real,vector<Rect>& buffer_rect, int i_img){
 
     return result;
 }
+
+
 
 void readDisparityImages(String path_in,String path_out,String path_real){
     vector<cv::String> img_name;
@@ -138,8 +266,8 @@ void readDisparityImages(String path_in,String path_out,String path_real){
         //cout << img_real_name[i] << endl;
         Mat img_real = imread(img_real_name[i]);
         //cout << "watafas" << endl;
-        imshow("disparity_img",img);
-        waitKey(0);
+        //imshow("disparity_img",img);
+        //waitKey(0);
         filterDisparity(img);
         Mat blob;
         blob = blobSelection(img,img_real,buffer_rect,i);
